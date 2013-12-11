519429780581df5042d462e023817d55a7709537
Rohan <rohan.dalvi@me.com>
2013-12-11 15:05:30 -0500

    Bug fixes and few advancements..

--- App.js
+++ App.js
@@ -6,6 +6,15 @@ var lumenize = window.parent.Rally.data.lookback.Lumenize;
 var iterationComboField = null;
 var iterationRecord = myMask = null;
 var setOfStories = setOfFeatures = null;
+var countFeatures = 0;
+var features = {
+	"project": "/project/7306522812", //Unity Product Family Requirements: 
+	"workspace": "/workspace/3181574357" //USD
+};
+var stories = {
+	"project": "/project/14355819910", //Teams
+	"workspace": "/workspace/3181574357" //USD
+};
 
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
@@ -31,25 +40,37 @@ Ext.define('CustomApp', {
         configs.push({ model : "Release",             
                        fetch : ['Name', 'ObjectID', 'Project', 'ReleaseStartDate', 'ReleaseDate' ],
                        context:{ //adding context while querying stories 
-		    				workspace: '/workspace/3181574357', //USD
-		    				project: '/project/14355819910' //USD Prtfolio:6020936452 , Teams: 14355819910
+		    				workspace: stories["workspace"], //USD
+		    				project: stories["project"] //USD Prtfolio:6020936452 , Teams: 14355819910
 		    			}, 
                        filters:[] 
         });
         configs.push({ model : "Iteration",             
                        fetch : ['Name', 'ObjectID', 'Project', 'StartDate', 'EndDate' ], 
                        context:{ //adding context while querying stories 
-		    				workspace: '/workspace/2154600806', //USD
-		    				project: '/project/11838764022' //USD Prtfolio:6020936452 , Teams: 14355819910
+		    				workspace: stories["workspace"], //USD
+		    				project: stories["project"] //USD Prtfolio:6020936452 , Teams: 14355819910
 		    			},
                        filters:[] 
         });
-        
+        configs.push({ model : "PreliminaryEstimate", 
+                       fetch : ['Name','ObjectID','Value'], 
+                       
+                       filters : [] 
+        });
+        configs.push({ model : "Tag",             
+                       fetch : ['Name', 'ObjectID'], 
+                       
+                       filters:[ { property : "Name", operator : "Contains" , value : "UPLC" } ] 
+        });
         
         async.map( configs, this.wsapiQuery, function(err,results) {
            
             that.releases  = results[0];
             that.iterations = results[1];
+            that.peRecords = results[2];
+            that.estimationTags = _.pluck(results[3],function(t){return t.get("ObjectID");});
+            console.log("Estimation tags ",that.estimationTags);
            
             if (showAssignedProgram)
                 that.createAssignedProgramCombo();
@@ -85,6 +106,10 @@ Ext.define('CustomApp', {
             stateful : true,
             stateId : "assignedProgramCombo",
             noData: false,
+            context:{
+            	workspace: features["workspace"],
+            	project: features["project"]
+            },
             listeners:{
             	scope: this,
             	change: function(field,eOpts){	
@@ -186,8 +211,11 @@ Ext.define('CustomApp', {
     		configs.push({
     			model: 'PortfolioItem/Feature',
     			fetch: ['ObjectID','FormattedID','UserStories' ],
-    			filters: [],//filter
-    			
+    			filters: [filter],//filter
+    			context:{
+    				workspace: features["workspace"],
+    				project: features["project"]
+    			},
     			listeners: {
                 load: function(store, features) {
                 	setOfFeatures = features;
@@ -219,8 +247,8 @@ Ext.define('CustomApp', {
     				value: null
     			}],
     			context:{ //adding context while querying stories 
-    				workspace: '/workspace/2154600806', //USD:3181574357, Workspace1: 2154600806
-    				project: '/project/11838764022' //USD Prtfolio:6020936452 , Teams: 14355819910, Ray-test: 11838764022
+    				workspace: stories["workspace"], //USD:3181574357, Workspace1: 2154600806
+    				project: stories["project"] //USD Prtfolio:6020936452 , Teams: 14355819910, Ray-test: 11838764022
     			},
     			listeners: {
     				load: function(store, stories){
@@ -261,7 +289,7 @@ Ext.define('CustomApp', {
 
     			if(candidateStories!=null){
     			
-    			that.getStorySnapShotsForFeatures();
+    			that.getStorySnapShotsForFeatures(setOfFeatures);
     			}
     			//create snapshot store based on candidateStories.
     			
@@ -269,14 +297,145 @@ Ext.define('CustomApp', {
     		});
 
     },
-    getStorySnapShotsForFeatures: function(){
-    	
+    getStorySnapShotsForFeatures: function(features){
+    	console.log("getStorySnapShotsForFeaturesInside ");
+    	var snapshots = [];
+        var that = this;
+        
+        async.map( features, this.readFeatureSnapshots, function(err,results) {
+            console.log("results",results);
+            _.each(results,function(result) {
+               //snapshots = snapshots.concat(result);
+            	snapshots=snapshots.concat(result);
+            });
+            console.log("total snapshots before",snapshots.length);
+           
+            // filter out stories that have an estimation tag        
+             snapshots = _.filter(snapshots,function(snapshot) {
+             	
+            	 //console.log("Intersections ",_.intersection(snapshot.get("Tags"), that.estimationTags ).length);
+                 return _.intersection(snapshot.get("Tags"), that.estimationTags ).length === 0;
+             });
+            console.log("total snapshots after",snapshots.length);
+          	
+            that.createChart2(snapshots,that.releases,that.start,that.end);
+        });
     },
     
     startDate: function(iterations){
     	var start = _.min(_.pluck(iterations, function(r){return r.get("StartDate");}));
     	return Rally.util.DateTime.toIsoString(start, false);
-    }
+    },
+    createChart2 : function ( snapshots, releases,start,end) {
+            
+        var that = this;
+        // var lumenize = window.parent.Rally.data.lookback.Lumenize;
+        var snapShotData = _.map(snapshots,function(d){return d.data;});
+        console.log("snapshots",snapShotData);
+        // can be used to 'knockout' holidays
+        var holidays = [
+        ];
+        var myCalc = Ext.create("storycalculator");
+
+        // calculator config
+        var config = {
+            deriveFieldsOnInput: myCalc.getDerivedFieldsOnInput(),
+            metrics: myCalc.getMetrics(),
+            summaryMetricsConfig: [],
+            deriveFieldsAfterSummary: myCalc.getDerivedFieldsAfterSummary(),
+            granularity: lumenize.Time.DAY,
+            tz: 'America/Chicago',
+            holidays: holidays,
+            workDays: 'Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday'
+        };
+        // release start and end dates
+        var startOnISOString = new lumenize.Time(start).getISOStringInTZ(config.tz);
+        console.log("isoStart",startOnISOString);
+        var upToDateISOString = new lumenize.Time(end).getISOStringInTZ(config.tz);
+        // create the calculator and add snapshots to it.
+        calculator = new lumenize.TimeSeriesCalculator(config);
+        calculator.addSnapshots(snapShotData, startOnISOString, upToDateISOString);
+        
+        // create a high charts series config object, used to get the hc series data
+        var hcConfig = [{ name : "label" }, 
+                        this.pointsUnitType() ? { name : "Planned Points" } : { name : "Planned Count" }, 
+                        { name : "PreliminaryEstimate"},
+                        this.pointsUnitType() ? { name : "Accepted Points"} : { name : "Accepted Count"},
+                        this.pointsUnitType() ? { name : "ProjectionPoints"}: { name : "ProjectionCount"},
+                        // { name : "Count", type:'column'},
+                        // { name : "Completed",type:'column'} 
+                        ];
+        var hc = lumenize.arrayOfMaps_To_HighChartsSeries(calculator.getResults().seriesData, hcConfig);
+        
+        this._showChart(hc);
+    },
+    readFeatureSnapshots : function(feature,callback) {
+    	
+        var that = this;
+        countFeatures++;
+        console.log("count is ",feature);
+        feature.getCollection("UserStories").load({
+            fetch : ["ObjectID"],
+            callback : function(records,operation,success) {
+            	console.log("User stories of features ",feature.getCollection("UserStories"));
+                console.log("Feature:"+feature.get("FormattedID"),records.length,records);
+                async.map(records,app.readParentStorySnapshots,function(err,results) {
+                    var snapshots = new Array();
+                    _.each(results,function(r) {
+                      snapshots=snapshots.concat(r);
+                    });
+                    callback(null,snapshots);    
+                });
+                
+            }
+        });
+        
+      
+     },
+      readParentStorySnapshots : function(parent,callback) {
+        console.log("Inside readParentStorySnapshots ");
+        Ext.create('Rally.data.lookback.SnapshotStore', {
+            limit : "Infinity",
+            autoLoad : true,
+            listeners: {
+                scope : this,
+                load: function(store, data, success) {
+                    callback(null,data);
+                }
+            },
+            fetch : ['Project', 'ScheduleState', 'PlanEstimate','Children','_ItemHierarchy','Tags'],
+            hydrate : ['ScheduleState'],
+            filters: [
+                {
+                    property: '_TypeHierarchy',
+                    operator: 'in',
+                    value: ['HierarchicalRequirement']
+                },
+                {
+                    property: '_ItemHierarchy',
+                    operator: 'in',
+                    value: [parent.get("ObjectID")]
+                },
+                // {
+                //     property: '_ValidTo',
+                //     operator: '>',
+                //     value: isoStart
+                // },
+                {
+                    property: 'Children',
+                    operator: '=',
+                    value: null
+                },
+                {
+                    property: '__At',
+                    operator: '=',
+                    value: 'current'
+                }
+
+            ]
+        });
+
+    },
     
     	
 });
